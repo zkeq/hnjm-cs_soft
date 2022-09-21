f12014614d17e1ae1a90bcde889c737aeb2f7744
Zkeq <admin@icodeq.com>
2022-09-21 05:22:15 +0000

    修复不能正确识别c# 代码块的 bug

--- components/NotionPage.tsx
+++ components/NotionPage.tsx
@@ -45,7 +45,7 @@ import styles from './styles.module.css'
 // -----------------------------------------------------------------------------
 
 const Code = dynamic(() =>
-  import('react-notion-x/build/third-party/code').then(async (m) => {
+  import('react-notion-zkeq/build/third-party/code').then(async (m) => {
     // add / remove any prism syntaxes here
     await Promise.all([
       import('prismjs/components/prism-markup-templating.js'),
