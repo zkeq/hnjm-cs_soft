c6fa4e783e1cc599781ba4726e78e64257b46235
Zkeq <admin@icodeq.com>
2022-09-21 05:26:34 +0000

    修复一个vercel的报错

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -30,8 +30,7 @@ import {
   isServer,
   fathomId,
   fathomConfig,
-  posthogId,
-  posthogConfig
+  posthogId
 } from 'lib/config'
 
 if (!isServer) {
@@ -55,11 +54,7 @@ export default function App({ Component, pageProps }: AppProps) {
     if (fathomId) {
       Fathom.load(fathomId, fathomConfig)
     }
-
-    if (posthogId) {
-      posthog.init(posthogId, posthogConfig)
-    }
-
+    
     router.events.on('routeChangeComplete', onRouteChangeComplete)
 
     return () => {
